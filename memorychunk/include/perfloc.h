d1ee9b95f8567007443fc54a4d8fb0956b144946
ashwinhprasad <ashwinprasad202@gmail.com>
2024-07-25 01:22:08 +0530

    Perfloc Implementation

--- memorychunk/include/perfloc.h
+++ memorychunk/include/perfloc.h
@@ -4,13 +4,13 @@
 
 #include "memory_chunk.h"
 
-typedef struct AllocObj {
+typedef struct AllocObjHeader {
     size_t size;
     ChildMeta* pmc_meta_list_object;
-    void* object_address;
-} AllocObj;
+} AllocObjHeader;
 
 #define ALLOC_OBJECT_OBJECT_ADDRESS_OFFSET sizeof(size_t) + sizeof(ChildMeta*)
+#define ALLOC_OBJECT_SIZE sizeof(AllocObjHeader)
 
 void* perfalloc(MemoryChunk, size_t);
 void perffree(MemoryChunk,void*);
--- memorychunk/src/perfloc.c
+++ memorychunk/src/perfloc.c
@@ -1,5 +1,6 @@
 #include "perfloc.h"
 #include "memory_chunk.h"
+#include "string.h"
 
 MemoryChunk getPerfMem()
 {
@@ -17,77 +18,44 @@ void perffree(MemoryChunk mc, void* object)
 
 }
 
-void* perfalloc(MemoryChunk pmc, size_t size) {
-    return NULL;
+void* perfalloc(MemoryChunk pmc, size_t object_size) 
+{
+    Header* header = pmc.header;
+    size_t allocating_object_size = ALLOC_OBJECT_SIZE + object_size;
+
+    if (header->occupied_space + allocating_object_size > header->total_size)
+    {
+        /*
+            Case where process memory chunk size is not enough.
+            I need to subtract meta list and header sizes from the
+            total size before comparison.
+        */
+    }
+
+    ChildMeta* current_child_meta = pmc.head_child_meta;
+    while (current_child_meta->next_child_meta != NULL)
+    {
+
+    }
+
+    void* object_alloc_address = current_child_meta->object_ptr + current_child_meta->size;
+    ChildMeta* object_child_meta = current_child_meta->object_ptr + CHILD_META_SIZE; 
+
+    AllocObjHeader alloc_obj = {
+        allocating_object_size,
+        object_child_meta
+    };
+    memcpy(object_alloc_address, &alloc_obj, ALLOC_OBJECT_SIZE);
+
+    object_child_meta->previous_child_meta = current_child_meta;
+    object_child_meta->next_child_meta = current_child_meta->next_child_meta;
+    if (current_child_meta->next_child_meta != NULL)
+    {
+        current_child_meta->next_child_meta->previous_child_meta = object_child_meta;
+    }
+    current_child_meta->next_child_meta = object_child_meta;
+    object_child_meta->object_ptr = object_alloc_address;
+    object_child_meta->is_head = false;
+    object_child_meta->size = allocating_object_size;
+    return object_alloc_address + ALLOC_OBJECT_SIZE;
 }
-
-// void* perfalloc(MemoryChunk pmc, size_t size)
-// {
-// 	void* memory = pmc.memory;
-// 	Header* pmc_header = (Header*) memory;
-// 	size_t memory_chunk_size = pmc_header->total_size;
-// 	size_t memory_meta_vec_size = memory_chunk_size * MEMORY_CHUNK_HEADER_SIZE_FACTOR;
-// 	void* alloc_region_start_address = memory + memory_meta_vec_size;
-// 	ObjectMeta* pmc_rom = memory + MC_HEADER_SIZE;
-
-// 	/**
-//      * Need to handle the case where memory chunk is not enough
-//     */
-// 	if (pmc_header->occupied_space + size > memory_chunk_size)
-// 	{
-
-// 	}
-
-
-// 	ObjectMeta* current_object_meta = pmc_rom;
-// 	void* previous_object_end_address = NULL;
-// 	void* previous_object_meta_start_address = NULL;
-// 	void* next_object_meta_start_address = NULL;
-// 	void* previous_object_meta_end_address = NULL;
-// 	bool is_object_meta_insertion_location_found = false;
-
-// 	while(current_object_meta != NULL)
-// 	{
-// 		if (current_object_meta->is_head)
-// 		{
-// 			previous_object_end_address = alloc_region_start_address;
-// 			previous_object_meta_end_address = current_object_meta + OBJECT_META_SIZE;
-// 			previous_object_meta_start_address = current_object_meta;
-// 			next_object_meta_start_address = current_object_meta->next_object_meta;
-// 		}
-// 		else if ((previous_object_end_address + size < current_object_meta->start_address) && (previous_object_meta_end_address + OBJECT_META_SIZE < (void *)current_object_meta))
-// 		{
-// 			break;
-// 		}
-// 		else
-// 	{
-// 			previous_object_end_address = current_object_meta->start_address + current_object_meta->size;
-// 			previous_object_meta_end_address = current_object_meta + OBJECT_META_SIZE;
-// 			previous_object_meta_start_address = current_object_meta;
-// 			next_object_meta_start_address = current_object_meta->next_object_meta;
-// 		}
-
-// 		current_object_meta = current_object_meta->next_object_meta;
-// 	}
-
-// 	/**
-//      * TODO:Also need to create new object meta for the current object
-//     */
-// 	void* object_meta_location = previous_object_meta_end_address;
-// 	void* object_location = previous_object_end_address;
-
-// 	ObjectMeta* object_meta = (ObjectMeta*) object_meta_location;
-// 	object_meta->start_address = object_location;
-// 	object_meta->is_head = false;
-// 	object_meta->size = size;
-// 	object_meta->next_object_meta = next_object_meta_start_address;
-// 	object_meta->previous_object_meta = previous_object_meta_start_address;
-// 	object_meta->previous_object_meta->next_object_meta = object_meta;
-
-// 	if (object_meta->next_object_meta != NULL)
-// 	{
-// 		object_meta->next_object_meta->previous_object_meta = object_meta;
-// 	}
-
-// 	return object_location;
-// }
